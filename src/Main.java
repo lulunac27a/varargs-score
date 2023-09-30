f8eac6f9298c5f0eb8be7b418072d27457caba9e
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2023-09-29 20:01:43 -0500

    Add score multiplier mode

--- src/Main.java
+++ src/Main.java
@@ -19,24 +19,46 @@ public class Main {
         return score;
     }
 
+    private static int multiple(ArrayList<Integer> args) {
+        int score = 0;
+        int multiplier = 1;
+        for (int i = 0; i < args.size(); i++) {
+            score += args.get(i) * multiplier;
+            multiplier++;
+        }
+        return score;
+    }
+
     public static void main(String[] args) {
         int exit = 0;
         ArrayList<Integer> scores = new ArrayList<>();
-
+        System.out.println("Press 1 for original, 2 for multiplier");
+        Scanner sc = new Scanner(System.in);
+        int mode = sc.nextInt();
         while (exit != 1) {
-            System.out.println("Press 1 to increase combo and increase score, 2 to reset combo, 0 to end");
-
-            Scanner sc = new Scanner(System.in);
-            int number = sc.nextInt();
-            if (number == 0) {
-                exit = 1;
-                System.out.println("Score: " + score(scores));
-            }
-            if (number == 1) {
-                scores.add(1);
-            }
-            if (number == 2) {
-                scores.add(0);
+            if (mode == 1) {
+                System.out.println("Press 1 to increase combo and increase score, 2 to reset combo, 0 to end");
+                int number = sc.nextInt();
+                if (number == 0) {
+                    exit = 1;
+                    System.out.println("Score: " + score(scores));
+                }
+                if (number == 1) {
+                    scores.add(1);
+                }
+                if (number == 2) {
+                    scores.add(0);
+                }
+            } else if (mode == 2) {
+                System.out.println("Enter 1-9 to add score and increase multiplier, 0 to end");
+                int number = sc.nextInt();
+                if (number == 0) {
+                    exit = 1;
+                    System.out.println("Score: " + multiple(scores));
+                } else {
+                    int value = Character.getNumericValue(String.valueOf(Math.abs(Math.max(1, number))).charAt(0));
+                    scores.add(value);
+                }
             }
         }
 
