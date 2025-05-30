277691c204c2436feea9c86e881112b0caa2e804
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-05-30 15:13:13 -0500

    Fix code in Kotlin and Scala

--- src/NumberLoop.kt
+++ src/NumberLoop.kt
@@ -1,40 +1,38 @@
 import java.util.*
-import kotlin.math.abs
 import kotlin.math.floor
 import kotlin.math.ln
-import kotlin.math.max
 
 object NumberLoop {
-    private fun score(args: ArrayList<Int>): Int {
-        var score = 0 // initialize the values
+    private fun score(args: ArrayList<Int?>): Int {
+        var score = 0 //initialize the values
         var combo = 0
 
         for (i in args.indices) {
-            if (args[i] == 1) { // add score and increase combo if the value is 1
+            if (args.get(i) == 1) { //add score and increase combo if the value is 1
                 combo++
                 score += combo
-            } else if (args[i] == 0) { // reset combo to 0
+            } else if (args.get(i) == 0) { //reset combo to 0
                 combo = 0
             }
         }
         return score
     }
 
-    private fun calculateScoreWithMultiplier(args: ArrayList<Int>): Int {
-        var score = 0 // initialize the values
+    private fun calculateScoreWithMultiplier(args: ArrayList<Int?>): Int {
+        var score = 0 //initialize the values
         var multiplier = 1
-        for (i in args.indices) { // add score and increase multiplier
-            score += args[i] * multiplier
+        for (i in args.indices) { //add score and increase multiplier
+            score += args.get(i)!! * multiplier
             multiplier++
         }
         return score
     }
 
-    private fun calculateScoreWithExponentialMultiplier(args: ArrayList<Int>): Double {
-        var score = 0.0 // initialize the values
-        var multiplier = 1.0 // multiplier is exponential
-        for (i in args.indices) { // add score and increase multiplier exponentially
-            score += args[i] * multiplier
+    private fun calculateScoreWithExponentialMultiplier(args: ArrayList<Int?>): Double {
+        var score = 0.0 //initialize the values
+        var multiplier = 1.0 //multiplier is exponential
+        for (i in args.indices) { //add score and increase multiplier exponentially
+            score += args.get(i)!! * multiplier
             multiplier += multiplier / (1 + ln(multiplier))
         }
         return floor(score)
@@ -43,14 +41,15 @@ object NumberLoop {
     @JvmStatic
     fun main(args: Array<String>) {
         var exit = false
-        val scores = ArrayList<Int>() //get the list to pass the methods with varargs
+        val scores = ArrayList<Int?>() //get the list to pass the methods with varargs
         println("Press 1 for original, 2 for multiplier, 3 for multiplier with exponential growth")
         val sc = Scanner(System.`in`) //get input
         val mode = sc.nextInt() //get input for mode
         while (!exit) {
             if (mode == 1) { //score and combo mode - enter 1 and 2
                 println("Press 1 to increase combo and increase score, 2 to reset combo, 0 to end")
-                val number = sc.nextInt() //get input for 0, 1 or 2
+                val numberStr = sc.next() //get input for 0, 1 or 2
+                val number = numberStr.toInt()
                 if (number == 0) {
                     exit = true //exit the program
                     println("Score: " + score(scores)) //print the final score
@@ -64,37 +63,23 @@ object NumberLoop {
             }
             if (mode == 2) { //score multiplier mode - enter 1 to 9
                 println("Enter 1-9 to add score and increase multiplier, 0 to end")
-                val number = sc.nextInt() //get input and then get the first digit of integer input
-                if (number == 0) {
+                val numberStr = sc.next() //get input and then get the first digit of integer input
+                if ("0" == numberStr) {
                     exit = true //exit the program
                     println("Score: " + calculateScoreWithMultiplier(scores)) //print the final score
                 } else {
-                    val value = Character.getNumericValue(
-                        abs(
-                            max(
-                                1.0,
-                                number.toDouble()
-                            )
-                        ).toString()[0]
-                    ) //get the numeric value between 1 and 9
+                    val value = Character.getNumericValue(numberStr.get(0)) //get the numeric value between 1 and 9
                     scores.add(value) //add the score and increase multiplier by 1
                 }
             }
             if (mode == 3) { //score multiplier mode with exponential growth - enter 1 to 9
                 println("Enter 1-9 to add score and increase multiplier, 0 to end")
-                val number = sc.nextInt() //get input and then get the first digit of integer input
-                if (number == 0) {
+                val numberStr = sc.next() //get input and then get the first digit of integer input
+                if ("0" == numberStr) {
                     exit = true //exit the program
                     println("Score: " + calculateScoreWithExponentialMultiplier(scores)) //print the final score
                 } else {
-                    val value = Character.getNumericValue(
-                        abs(
-                            max(
-                                1.0,
-                                number.toDouble()
-                            )
-                        ).toString()[0]
-                    ) //get the numeric value between 1 and 9
+                    val value = Character.getNumericValue(numberStr.get(0)) //get the numeric value between 1 and 9
                     scores.add(value) //add the score and increase multiplier exponentially
                 }
             }
--- src/NumberLoop.scala
+++ src/NumberLoop.scala
@@ -4,15 +4,15 @@ import java.util.{ArrayList, Scanner}
 
 object NumberLoop {
   private def score(args: util.ArrayList[Integer]) = {
-    var score = 0 // initialize the values
+    var score = 0 //initialize the values
 
     var combo = 0
     for (i <- 0 until args.size) {
-      if (args.get(i) eq 1) { // add score and increase combo if the value is 1
+      if (args.get(i) eq 1) { //add score and increase combo if the value is 1
         combo += 1
         score += combo
       }
-      else if (args.get(i) eq 0) { // reset combo to 0
+      else if (args.get(i) eq 0) { //reset combo to 0
         combo = 0
       }
     }
@@ -20,10 +20,10 @@ object NumberLoop {
   }
 
   private def calculateScoreWithMultiplier(args: util.ArrayList[Integer]) = {
-    var score = 0 // initialize the values
+    var score = 0 //initialize the values
 
     var multiplier = 1
-    for (i <- 0 until args.size) { // add score and increase multiplier
+    for (i <- 0 until args.size) { //add score and increase multiplier
       score += args.get(i) * multiplier
       multiplier += 1
     }
@@ -31,11 +31,11 @@ object NumberLoop {
   }
 
   private def calculateScoreWithExponentialMultiplier(args: util.ArrayList[Integer]) = {
-    var score = 0 // initialize the values
+    var score = 0 //initialize the values
 
-    var multiplier = 1 // multiplier is exponential
+    var multiplier = 1 //multiplier is exponential
 
-    for (i <- 0 until args.size) { // add score and increase multiplier exponentially
+    for (i <- 0 until args.size) { //add score and increase multiplier exponentially
       score += args.get(i) * multiplier
       multiplier += multiplier / (1 + Math.log(multiplier))
     }
@@ -54,8 +54,9 @@ object NumberLoop {
     while (!exit) {
       if (mode == 1) { //score and combo mode - enter 1 and 2
         System.out.println("Press 1 to increase combo and increase score, 2 to reset combo, 0 to end")
-        val number = sc.nextInt //get input for 0, 1 or 2
+        val numberStr = sc.next //get input for 0, 1 or 2
 
+        val number = numberStr.toInt
         if (number == 0) {
           exit = true //exit the program
 
@@ -67,16 +68,16 @@ object NumberLoop {
       }
       if (mode == 2) { //score multiplier mode - enter 1 to 9
         System.out.println("Enter 1-9 to add score and increase multiplier, 0 to end")
-        val number = sc.nextInt //get input and then get the first digit of integer input
+        val numberStr = sc.next //get input and then get the first digit of integer input
 
-        if (number == 0) {
+        if ("0" == numberStr) {
           exit = true //exit the program
 
           System.out.println("Score: " + calculateScoreWithMultiplier(scores)) //print the final score
 
         }
         else {
-          val value = Character.getNumericValue(String.valueOf(Math.abs(Math.max(1, number))).charAt(0)) //get the numeric value between 1 and 9
+          val value = Character.getNumericValue(numberStr.charAt(0)) //get the numeric value between 1 and 9
 
           scores.add(value) //add the score and increase multiplier by 1
 
@@ -84,16 +85,16 @@ object NumberLoop {
       }
       if (mode == 3) { //score multiplier mode with exponential growth - enter 1 to 9
         System.out.println("Enter 1-9 to add score and increase multiplier, 0 to end")
-        val number = sc.nextInt //get input and then get the first digit of integer input
+        val numberStr = sc.next //get input and then get the first digit of integer input
 
-        if (number == 0) {
+        if ("0" == numberStr) {
           exit = true //exit the program
 
           System.out.println("Score: " + calculateScoreWithExponentialMultiplier(scores)) //print the final score
 
         }
         else {
-          val value = Character.getNumericValue(String.valueOf(Math.abs(Math.max(1, number))).charAt(0)) //get the numeric value between 1 and 9
+          val value = Character.getNumericValue(numberStr.charAt(0)) //get the numeric value between 1 and 9
 
           scores.add(value) //add the score and increase multiplier exponentially
 
